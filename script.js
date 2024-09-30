c9169fc6f0fee5df0e627f7eec656138b2a3b0d2
Věra Juračáková <juracakova.vera@gmail.com>
2024-09-30 20:25:20 +0200

    Dokonceni ukolu

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //ÚKOL č. 1: Příjem divadla
-document.body.innerHTML += "<p>" + "ÚKOL č. 1" + "</p>"
+document.body.innerHTML += "<p>" + "ÚKOL: Příjem divadla" + "</p>"
 
 const listek = 12
 const navstevnik = 174
@@ -11,3 +11,12 @@ const prijemS = Math.ceil((cenaS * (0.4 * navstevnik))* 15) + Math.ceil((listek 
 document.body.innerHTML += "<p>" + "Příjem divadla Pěst je " + prijem + " EUR" + "</p>"
 
 document.body.innerHTML += "<p>" + "Příjem divadla se studentskou slevou činí " + prijemS + " EUR" + "</p>"
+
+
+//ÚKOL č. 2: Házení kostkou
+document.body.innerHTML += "<p>" + "ÚKOL: Házení kostkou" + "</p>"
+
+const hod = Math.floor(Math.random() * 6) + 1
+
+
+document.body.innerHTML += "<p>" + "Na kostce padlo číslo " + hod + "</p>"
